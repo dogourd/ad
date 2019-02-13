0456e19c1afc6fb9b5f28108dc1b42caf28fa167
yuwen <yuwen2424@163.com>
2019-02-13 19:43:18 +0800

    测试controller部分create/user出现问题待解决

---
+++ ad-service/ad-sponsor/src/test/java/top/ezttf/ad/config/RestTemplateConfiguration.java
@@ -0,0 +1,8 @@
+package top.ezttf.ad.config;
+
+/**
+ * @author yuwen
+ * @date 2019/2/13
+ */
+public class RestTemplateConfiguration {
+}
---
+++ ad-service/ad-sponsor/src/test/java/top/ezttf/ad/controller/UserControllerTest.java
@@ -0,0 +1,8 @@
+package top.ezttf.ad.controller;
+
+/**
+ * @author yuwen
+ * @date 2019/2/13
+ */
+public class UserControllerTest {
+}
